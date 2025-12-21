cba99caff3e1fc9942fa499516f68bf89638b936
EHSAN <course.online.ehsan@gmail.com>
2025-12-21 08:27:51 +0500

    Create ResultTable.tsx

---
+++ src/components/ResultTable.tsx
@@ -0,0 +1,24 @@
+export default function ResultTable({ result }: any) {
+  return (
+    <table border={1} cellPadding={8}>
+      <thead>
+        <tr>
+          <th>Heir</th>
+          <th>Fraction</th>
+          <th>Amount</th>
+          <th>Reason</th>
+        </tr>
+      </thead>
+      <tbody>
+        {result.map((r: any, i: number) => (
+          <tr key={i}>
+            <td>{r.heir}</td>
+            <td>{r.fraction}</td>
+            <td>{r.amount.toFixed(2)}</td>
+            <td>{r.reason}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}

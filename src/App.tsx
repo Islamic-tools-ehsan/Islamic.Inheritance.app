488165026d7402f03e0942c0958336576f4ad37c
EHSAN <course.online.ehsan@gmail.com>
2025-12-21 08:42:39 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
+
 import { useState } from "react";
 import HeirInput from "./components/HeirInput";
 import ResultTable from "./components/ResultTable";
-import { calculateHanafi } from "./logic/hanafiEngine";
+import FiqhSelector from "./components/FiqhSelector";
+import { calculateInheritance } from "./logic/inheritanceEngine";
 
 export default function App() {
   const [estate, setEstate] = useState(1000000);
+  const [fiqh, setFiqh] = useState("hanafi");
+
   const [heirs, setHeirs] = useState({
+    husband: 0,
     wife: 1,
     son: 0,
     daughter: 1,
     father: 0,
-    mother: 0
+    mother: 0,
+    brother: 0,
+    sister: 0
   });
 
-  const result = calculateHanafi(estate, heirs);
+  const result = calculateInheritance(estate, heirs, fiqh);
 
   return (
-    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
-      <h1>Islamic Inheritance Calculator (Hanafi)</h1>
+    <div style={{ padding: 20 }}>
+      <h1>Islamic Inheritance Calculator</h1>
 
+      <FiqhSelector value={fiqh} onChange={setFiqh} />
+
+      <br />
       <label>Estate Amount</label><br />
       <input
         type="number"

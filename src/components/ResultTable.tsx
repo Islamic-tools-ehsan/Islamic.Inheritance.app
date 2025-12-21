6c8da80b24925edeec3b0c162022c60bea6bd4fb
EHSAN <course.online.ehsan@gmail.com>
2025-12-21 14:06:01 +0500

    Update ResultTable.tsx

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -1,24 +1,36 @@
-export default function ResultTable({ result }: any) {
+import { exportToPDF } from "../utils/pdfExport";
+
+export default function ResultTable({ result, estate }: any) {
   return (
-    <table border={1} cellPadding={8}>
-      <thead>
-        <tr>
-          <th>Heir</th>
-          <th>Fraction</th>
-          <th>Amount</th>
-          <th>Reason</th>
-        </tr>
-      </thead>
-      <tbody>
-        {result.map((r: any, i: number) => (
-          <tr key={i}>
-            <td>{r.heir}</td>
-            <td>{r.fraction}</td>
-            <td>{r.amount.toFixed(2)}</td>
-            <td>{r.reason}</td>
+    <div>
+      <button
+        onClick={() => exportToPDF(result, estate)}
+        style={{ marginBottom: "10px" }}
+      >
+        Export PDF
+      </button>
+
+      <table border={1} cellPadding={8}>
+        <thead>
+          <tr>
+            <th>Heir</th>
+            <th>Fraction</th>
+            <th>Amount</th>
+            <th>Reason</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {result.map((r: any, i: number) => (
+            <tr key={i}>
+              <td>{r.heir}</td>
+              <td>{r.fraction}</td>
+              <td>{r.amount.toFixed(2)}</td>
+              <td>{r.reason}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
+

17dc4c5582848e5f963d3d6ef3a9ad8bd2b2d4b8
EHSAN <course.online.ehsan@gmail.com>
2025-12-23 10:35:16 +0500

    Update sw.js

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,1 +1,17 @@
-self.addEventListener("fetch", () => {});
+const CACHE = "inheritance-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll(["/", "/index.html"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(
+      res => res || fetch(e.request)
+    )
+  );
+});
